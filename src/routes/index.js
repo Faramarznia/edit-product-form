db1421fa25b64e52c40d060e2607fd763d9102c8
Faramarz Faramarznia <Faramarznia.developer@gmail.com>
2022-11-19 20:00:32 +0330

    add project

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import DefaultLayout from "layouts/Default";
+import EditProduct from "pages/edit-product";
+
+const indexRoutes = [{ path: "/", component: DefaultLayout }];
+const AppRoutes = [
+  { path: "products/:id/edit", component: EditProduct },
+  { path: "/", pathTo: "/products/1/edit", name: "Products", redirect: true },
+];
+
+export default AppRoutes;
+
+export { indexRoutes };
